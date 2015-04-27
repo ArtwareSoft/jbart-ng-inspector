cd6bc52e17581fcf93708e34a7518b8ab4b128b1
Yaniv Ben-Yehuda <yanivby@artwaresoft.com>
2015-04-27 16:02:11 +0300

    use ng-inspector-outer from the cloud

--- chrome-extension/ext-content-end.js
+++ chrome-extension/ext-content-end.js
@@ -21,7 +21,7 @@ function insertIFrame() {
 	document.write('#jb-placeholder { position: absolute; left:10px; right:10px; top: 40px; bottom: 10px; overflow: auto; }');
 	document.write('#jb-placeholder-inner { padding: 10px;  background: #fff; height: 100%; border: 1px solid #ccc; border-radius: 1px; }');
 	document.write('</style>');
-	document.write('<script src="http://localhost:8090/js/ng-inspector-outer.js" ></script>');
+	document.write('<script src="http://storage.googleapis.com/jbartlib/apps/jbart-ng-inspector/ng-inspector-outer.js" ></script>');
 
 
 	document.write('<div id="jb-all">outer<div id="jb-placeholder"><div id="jb-placeholder-inner"><iframe width="100%" height="100%" frameborder="0" src="'+window.location.href+'"/></div></div></div>');
